065441d2fc9c32a36e09ae78c7aa427425e6f8f9
June YAMAMOTO <june@yambal.net>
2021-06-12 01:34:04 +0900

    bootstrap

--- src/features/home/ComeContainer.tsx
+++ src/features/home/ComeContainer.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { 
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link 
-} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export const HomeContainer = () => {
   return <div>
-      <h1>Hello 2</h1>
-      <Link to="/counter">Counter</Link>
+      <h1>やぁ、世界</h1>
+      <p>いろはにおへどちりぬるを</p>
+      <h2>やぁ、世界</h2>
+      <p>いろはにおへどちりぬるを</p>
+      <h3>やぁ、世界</h3>
+      <p>いろはにおへどちりぬるを</p>
+      <h4>やぁ、世界</h4>
+      <p>いろはにおへどちりぬるを</p>
+      <h5>やぁ、世界</h5>
+      <p>いろはにおへどちりぬるを</p>
+      <h6>やぁ、世界</h6>
+      <p>いろはにおへどちりぬるを</p>
+      <hr />
+      <Link to="/counter">カウンタ</Link>
     </div>
 }
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,77 +1,40 @@
 export const theme = {
-  colors: {
-    bg: '#F7FAFC',
-    bgAlt: '#213B60',
-    fg: '#425269',
-    fgAlt: '#FFF',
-    line: '#557799',
-    lineSection: '#DFEAF2',
-    title: '#596682',
-    caption: '#8298AB',
-    link: '#425269',
-    normal: '#FFF',
-    primary: '#7DB5A0',
-    alert: '#F1516F',
-    calendarBg: '#7DB5A0',
-    calendarBgActive: '#40B287',
-    calendarBgDisabled: '#6D7278',
-    calendarBgAlt: '#FFF',
-    calendarLine: '#DEDEDE',
-    calendarLineAlt: '#7DB5A0',
-    inputBg: '#FFF',
-    inputLine: '#BFCED9',
-    inputPlaceholder: '#9DACBB',
-    videoBg: '#425269',
-  },
-
-  lineHeights: {
-    xsmall: 1.0,
-    small: 1.45,
-    normal: 1.5,
-  },
-
-  fonts: {
-    sans: 'Noto Sans JP', // NotoSansCJKjp
-    serif: 'Noto Serif JP', // NotoSerifCJKjp
+  font: {
+    base: "'Noto Sans JP', sans-serif",
+    system:
+      'system-ui, -apple-system, "Hiragino Sans", "Yu Gothic UI", "Segoe UI", "Meiryo", sans-serif',
   },
 
   fontSizes: {
-    xsmall: '11rpx',
-    small: '12rpx',
-    normal: '13rpx',
-    medium: '14rpx',
-    large: '20rpx',
-    xlarge: '30rpx',
-    xxlarge: '40rpx',
+    base: '1rem',
+    h6: '1rem',
+    h5: '1.25rem',
+    h4: 'calc(1.275rem + .3vw)',
+    h3: 'calc(1.3rem + .6vw)',
+    h2: 'calc(1.325rem + .9vw)',
+    h1: 'calc(1.375rem + 1.5vw)',
   },
 
-  radii: {
-    normal: '4rpx',
+  fontWeight: {
+    base: 400,
+    bold: 500
   },
 
-  space: {
-    s0: 0,
-    s1: '4rpx',
-    s2: '8rpx',
-    s3: '12rpx',
-    s4: '16rpx',
-    s5: '20rpx',
-    s6: '24rpx',
-    s7: '28rpx',
-    s8: '32rpx',
-    s9: '36rpx',
-    s10: '40rpx',
-    s11: '44rpx',
-    s12: '48rpx',
-    s13: '52rpx',
-    s14: '56rpx',
-    s15: '60rpx',
-    s16: '64rpx',
-    s17: '68rpx',
-    s18: '72rpx',
-    s19: '76rpx',
-    s20: '80rpx',
+  lineHeights: {
+    headingsLineHeight: '1.2rem',
+    base: '1.5rem'
   },
+
+  colors: {
+    inherit: 'inherit',
+    bodyColor: '#212529',
+    bodyBg: '#FFFFFF'
+  },
+
+  spaces: {
+    s05: '0.5rem',
+    s10: '1rem'
+  }
 }
 
 export type AppTheme = typeof theme
