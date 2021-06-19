bcc4ef4a1c842ea6313b82786335b1c5792439c1
June YAMAMOTO <june@yambal.net>
2021-06-20 04:32:50 +0900

    __roomMember

--- src/features/room/PeerContainer.tsx
+++ src/features/room/PeerContainer.tsx
@@ -1,140 +1,94 @@
-import React, { useCallback, useEffect, useMemo, useState,  } from 'react';
-import { x } from '@xstyled/styled-components'
-import { useMount, useUnmount } from 'react-use'
-import Peer, {MeshRoom} from 'skyway-js'
-import { useRecoilState } from 'recoil'
-import {
-  peerIsOpenState,
-  peerMyPeerIdState,
-  roomIsOpenState,
-  RoomMember,
-  roomMembersState,
-  RoomMemberState
-} from './atoms';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { useMount } from 'react-use';
+import Peer, { MeshRoom } from 'skyway-js';
 
-const DATA_TYPE = {
-  PING: 'ping',
-  RES_PING: 'res_ping'
+type RoomData = {
+  dataType: 'ping' | 'res_ping'
+  to: string | 'all'
+  name: string
 }
 
-export const PeerContainer = () => {
-  const [peerIsOpen, setPeerIsOpen] = useRecoilState(peerIsOpenState)
-  const [myPeerId, setMyPeerId] = useRecoilState(peerMyPeerIdState)
-  const [roomIsOpen, setRoomIsOpen] = useRecoilState(roomIsOpenState)
-  const [roomMembers, setRoomMembers] = useRecoilState(roomMembersState)
+const peer = new Peer({key: 'd7be2df2-a221-4b31-8777-4911bece8381', debug: 0});
+let room: MeshRoom = null;
+peer.on('error', console.error);
 
-  useUnmount(() => { 
-    if(room) {
-      room.close()
-    }
-    if(peer){
-      peer.destroy()
-    }
+let __roomMember = {}
+
+const ROOM_ID='1st'
+
+export const PeerContainer = () => {
+  const [roomMember, setRoomNumber] = useState({})
+
+  const joinSkyway = () => {
+    room = peer.joinRoom(ROOM_ID, {
+      mode: 'mesh'
+    });
+
+    room.once('open', () => {
+      console.log(`--- opened ---`)
+      console.log(room)
+
+      const data:RoomData = {
+        dataType: "ping",
+        to: 'all',
+        name: ''
+      }
+      console.log(`send: ${JSON.stringify(data)}`)
+      room.send(data)
+    });
+
+    room.on('data', ({ src, data }) => {
+      const from: string = src
+      const recieveData: RoomData = data
+      console.log(`data: ${JSON.stringify(recieveData)} from ${from}`)
+      switch(`${recieveData.dataType}-${recieveData.to}`){
+        case 'ping-all':
+          const data:RoomData = {
+            dataType: 'res_ping',
+            to: from,
+            name: ''
+          }
+          console.log(`send: ${JSON.stringify(data)}`)
+          room.send(data)
+          break
+        case `res_ping-${peer.id}`:
+          console.log(`res ping ${JSON.stringify(recieveData)} from ${from}`)
+          const member = {}
+          member[from] = {
+            name: recieveData.name
+          }
+          __roomMember = Object.assign({}, __roomMember, member)
+          setRoomNumber(__roomMember)
+          break
+      }
+    })
+
+    room.on('peerJoin', peerId => {
+      console.log(`--- ${peerId} joined ---`);
+    });
+    room.on('stream', stream => {
+      console.log(`--- ${stream.peerId} stream ---`);
+    });
+    room.on('peerLeave', peerId => {
+      console.log(`--- ${peerId} leaved ---`);
+    });
+    
+    room.once('close', () => {
+      console.log('--- leaved ---');
+    });
+  }
+
+  useMount(() => {
+    
+    joinSkyway()
   })
 
-  // Peer
-  const peer = useMemo(() => {
-    const _peer = new Peer({key: '42f75ed0-a9ff-4f07-ad83-cecc2daa274c'})
-    _peer.on('open', (peerId) => {
-      setPeerIsOpen(true)
-      setMyPeerId(peerId)
-    })
-    return _peer
-  },[])
+  console.log(roomMember)
 
-  useEffect(() => {
-    if(peerIsOpen) {
-      room.on('close', onPeerClose)
-    }
-  },[peerIsOpen])
-
-  const onPeerClose = useCallback(() => {
-    console.log(`peer close`)
-    setPeerIsOpen(false)
-    setMyPeerId(undefined)
-  },[peerIsOpen])
-
-  // Room
-  const room = useMemo(() => {
-    if(peerIsOpen){
-      const _room: MeshRoom = peer.joinRoom('4st')
-      _room.on('open', (peerId) => {
-        setRoomIsOpen(true)
-      })
-      return _room
-    } 
-    return undefined
-  },[peerIsOpen])
-
-  const myMemberData: RoomMember = useMemo(() => {
-    return {
-      peerId: myPeerId,
-      name: '',
-      position: {
-        x: 0,
-        y: 0
-      }
-    }
-  },[myPeerId])
-
-  useEffect(() => {
-    if(roomIsOpen) {
-      room.on('data', onData)
-      room.on('close',onRoomClose)
-      room.on('peerLeave', onPeerLeave)
-      room.send({
-        dataType: DATA_TYPE.PING,
-        member: myMemberData
-      })
-    }
-  },[roomIsOpen])
-
-  const onData = useCallback((catchData) => {
-    const dataType = catchData.data.dataType
-    const member: RoomMember = catchData.data.member
-    switch(dataType){
-      case DATA_TYPE.PING:
-        fetchMember(member)
-        room.send({
-          dataType: DATA_TYPE.RES_PING,
-          member: myMemberData
-        })
-        break
-      case DATA_TYPE.RES_PING:
-        fetchMember(member)
-        break
-    }
-  },[room])
-
-  const onRoomClose = useCallback(() => {
-    console.log(`room close`)
-    setRoomIsOpen(false)
-    setRoomMembers({})
-  },[])
-
-  const onPeerLeave = useCallback((peerId: string) => {
-    const newRoomMembers = Object.assign({}, roomMembers)
-    delete newRoomMembers[peerId]
-    setRoomMembers(newRoomMembers)
-  },[roomMembers])
-
-  const fetchMember = useCallback((member: RoomMember) => {
-    const fetchMenber: RoomMemberState = {}
-    fetchMenber[member.peerId] = member
-    const newRoomMembers = Object.assign({}, roomMembers, fetchMenber)
-    setRoomMembers(newRoomMembers)
-  },[roomMembers])
-
-  return(
-    <x.div>
-      <div>{peerIsOpen && "open"}</div>
-      {myPeerId && myPeerId}
-      <div>{roomIsOpen && "roomIsOpen"}</div>
-      <ul>
-        {Object.keys(roomMembers).map((m) => {
-          return <li key={roomMembers[m].peerId}>{roomMembers[m].peerId} : {roomMembers[m].position.x}</li>
-        })}
-      </ul>
-    </x.div>
-  )
+  return (
+    <div>
+      <div>{peer && peer.id}</div>
+      <div>{JSON.stringify(roomMember)}</div>
+    </div>
+  );
 }
